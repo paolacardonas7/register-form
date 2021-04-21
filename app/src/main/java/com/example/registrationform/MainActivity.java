4292afd09aabd0cb6640f37324c0ad27462460f5
paolacardonas7 <pao_cardona97@hotmail.com>
2021-04-21 16:53:55 -0400

    registration form done

--- app/src/main/java/com/example/registrationform/MainActivity.java
+++ app/src/main/java/com/example/registrationform/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.registrationform;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,19 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    public void onRegisterBtnClick(View view){
+        EditText edtTxtFirstName = findViewById(R.id.edtTxtFirstName);
+        EditText edtTxtName = findViewById(R.id.edtTxtName);
+        EditText edtTxtEmail = findViewById(R.id.edtTxtEmail);
+
+        TextView txtFirstName = findViewById(R.id.txtFirstName);
+        TextView txtName = findViewById(R.id.txtName);
+        TextView txtEmail = findViewById(R.id.txtEmail);
+
+        txtFirstName.setText(edtTxtFirstName.getText());
+        txtName.setText(edtTxtName.getText());
+        txtEmail.setText(edtTxtEmail.getText());
+    }
+
 }
